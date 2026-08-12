e6e4bf072b641c40df36e6dd648b959ccdd29bc7
Maykon2001 <maykoncarvalho2001@gmail.com>
2026-08-11 22:53:16 -0300

    Feito o exercicio 042

--- exe042.py
+++ exe042.py
@@ -1,1 +1,19 @@
-# Exercício 042 - 
+# Exercício 042 - Refaça o DESAFIO 035 dos triângulos, acrescentando o recuros de mostrar qe dtipo de triângulo seá formado: 
+
+print('-='*20)
+print('Analisador de Triângulos')
+print('-='*20)
+
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro seguimento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmento acima PODEM FORMAR UM TRIÂNGULO!')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO.')
+    elif r1 == r2 or r2 == r3 or r1 == r3:
+        print('ISÓSCELES.')
+    else:
+        print('ESCALENO.')
+else:
+    print('Os segmentos acima NÃO PODEM FOEMAR UM TRIÂNGULO!')
